966821faa63e41887d96081fff74e6394514b6b0
edouard974dev <e.willaume@gmail.com>
2025-07-21 18:51:08 +0200

    Add files via upload

---
+++ data 2.js
@@ -0,0 +1,37 @@
+
+// data.js – Données LRC complètes ERJ145
+
+const lrcData = {
+  "5000": {
+    "18000": { N1: 65.8, fuelFlow: 570, IAS: 248, TAS: 267, mach: 0.409, SR: 0.232 },
+    "20000": { N1: 66.2, fuelFlow: 560, IAS: 248, TAS: 268, mach: 0.410, SR: 0.237 },
+    "22000": { N1: 67.5, fuelFlow: 555, IAS: 249, TAS: 270, mach: 0.415, SR: 0.241 }
+  },
+  "10000": {
+    "18000": { N1: 70.6, fuelFlow: 581, IAS: 257, TAS: 298, mach: 0.465, SR: 0.256 },
+    "20000": { N1: 69.8, fuelFlow: 558, IAS: 253, TAS: 293, mach: 0.458, SR: 0.262 },
+    "22000": { N1: 68.4, fuelFlow: 546, IAS: 249, TAS: 288, mach: 0.453, SR: 0.265 }
+  },
+  "18000": {
+    "20000": { N1: 70.9, fuelFlow: 456, IAS: 231, TAS: 302, mach: 0.488, SR: 0.331 },
+    "22000": { N1: 75.6, fuelFlow: 554, IAS: 249, TAS: 325, mach: 0.524, SR: 0.294 },
+    "24000": { N1: 77.1, fuelFlow: 572, IAS: 252, TAS: 331, mach: 0.532, SR: 0.288 }
+  },
+  "22000": {
+    "20000": { N1: 70.9, fuelFlow: 519, IAS: 239, TAS: 361, mach: 0.616, SR: 0.353 },
+    "21000": { N1: 77.7, fuelFlow: 522, IAS: 241, TAS: 335, mach: 0.549, SR: 0.321 },
+    "22000": { N1: 79.0, fuelFlow: 549, IAS: 246, TAS: 341, mach: 0.559, SR: 0.311 }
+  },
+  "29000": {
+    "20000": { N1: 84.7, fuelFlow: 550, IAS: 246, TAS: 388, mach: 0.657, SR: 0.353 },
+    "22000": { N1: 85.9, fuelFlow: 554, IAS: 247, TAS: 402, mach: 0.688, SR: 0.363 },
+    "24000": { N1: 87.7, fuelFlow: 561, IAS: 249, TAS: 419, mach: 0.722, SR: 0.374 },
+    "25000": { N1: 89.0, fuelFlow: 565, IAS: 250, TAS: 428, mach: 0.741, SR: 0.379 }
+  },
+  "37000": {
+    "18000": { N1: 91.5, fuelFlow: 590, IAS: 243, TAS: 459, mach: 0.791, SR: 0.389 },
+    "20000": { N1: 92.4, fuelFlow: 595, IAS: 245, TAS: 466, mach: 0.801, SR: 0.393 },
+    "22000": { N1: 93.2, fuelFlow: 602, IAS: 247, TAS: 472, mach: 0.812, SR: 0.395 },
+    "24000": { N1: 94.0, fuelFlow: 609, IAS: 248, TAS: 479, mach: 0.825, SR: 0.398 }
+  }
+};
