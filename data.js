501a1cd3f2bd434a35a2197a8b71032281a3803b
edouard974dev <e.willaume@gmail.com>
2025-07-21 17:30:15 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,1 +1,19 @@
-// Données LRC complètes générées automatiquement = en cours
+// data.js – Données LRC extraites du manuel ERJ145 (extrait simplifié)
+
+const lrcData = {
+  "18000": {
+    "17000": { N1: 70.3, fuelFlow: 460, IAS: 232, TAS: 302, mach: 0.488, SR: 0.331 },
+    "20000": { N1: 70.9, fuelFlow: 456, IAS: 231, TAS: 308, mach: 0.497, SR: 0.331 },
+    "22000": { N1: 75.6, fuelFlow: 554, IAS: 249, TAS: 325, mach: 0.524, SR: 0.294 }
+  },
+  "22000": {
+    "20000": { N1: 70.9, fuelFlow: 519, IAS: 239, TAS: 361, mach: 0.616, SR: 0.353 },
+    "21000": { N1: 77.7, fuelFlow: 522, IAS: 241, TAS: 335, mach: 0.549, SR: 0.321 },
+    "22000": { N1: 79.0, fuelFlow: 549, IAS: 246, TAS: 341, mach: 0.559, SR: 0.311 }
+  },
+  "29000": {
+    "20000": { N1: 84.7, fuelFlow: 550, IAS: 246, TAS: 388, mach: 0.657, SR: 0.353 },
+    "22000": { N1: 85.9, fuelFlow: 554, IAS: 247, TAS: 402, mach: 0.688, SR: 0.363 },
+    "24000": { N1: 87.7, fuelFlow: 561, IAS: 249, TAS: 419, mach: 0.722, SR: 0.374 }
+  }
+};
